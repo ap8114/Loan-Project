7572155155ac86f517c8b64dd9372362187a2d1d
Arun-badode <arunbadode08@gmail.com>
2025-06-26 11:37:05 +0530

    first commit

--- src/Layout/Sidebar.jsx
+++ src/Layout/Sidebar.jsx
@@ -1,52 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import "./Sidebar.css"
 
-const Sidebar = () => {
+const Sidebar = ({ collapsed }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const role = localStorage.getItem("userRole"); // ✅ GET ROLE from localStorage
+
+  const isActive = (path) => location.pathname === path;
+
+  const menuItemClick = () => {
+    console.log("Menu item clicked");
+  };
+
   return (
-    <div className="d-flex min-vh-100 w-100">
-      {/* Sidebar */}
-      <div
-        className="d-flex flex-column align-items-center p-3 shadow"
-        style={{
-          width: '200px',
-          backgroundColor: '#E8F5E9',
-          height: '100vh',
-          position: 'sticky',
-          top: 0,
-          left: 0,
-          zIndex: 1020,
-        }}
-      >
-        {/* Logo */}
-        <div
-          className="rounded-circle bg-danger d-flex align-items-center justify-content-center mb-4 border border-white"
-          style={{ width: '64px', height: '64px' }}
-        >
-          <i className="fas fa-paw text-white fs-4"></i>
-        </div>
+    <div className={`sidebar-container ${collapsed ? "collapsed" : ""}`}>
+      <div className="sidebar bg-card " style={{ height: "101vh" }}>
+        <ul className="menu">
+          {/* Admin Only */}
 
-        {/* Navigation Buttons */}
-        <div className="w-100 px-2">
-          <button
-            className="btn w-100 mb-3 text-start text-dark fw-medium"
-            style={{
-              backgroundColor: '#9DDECE',
-              borderRadius: '999px',
-            }}
-          >
-            Dashboard
-          </button>
-
-          <button
-            className="btn w-100 text-start text-dark fw-medium"
-            style={{
-              borderRadius: '999px',
-            }}
-            onMouseOver={(e) => (e.target.style.backgroundColor = '#9DDECE')}
-            onMouseOut={(e) => (e.target.style.backgroundColor = 'transparent')}
-          >
-            Log out
-          </button>
-        </div>
+          <>
+            {/* Dashboard */}
+            <li
+              className={`menu-item ${
+                isActive("/admin-dashboard") ? "active" : ""
+              }`}
+            >
+              <div
+                className="menu-link menu-i"
+                onClick={() => {
+                  navigate("/admin-dashboard");
+                  menuItemClick();
+                }}
+              >
+                <i className="fa-solid fa-compass"></i>
+                <span className="menu-text">Dashboard</span>
+              </div>
+            </li>
+          </>
+        </ul>
       </div>
     </div>
   );
